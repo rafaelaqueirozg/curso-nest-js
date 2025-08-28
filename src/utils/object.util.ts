38c87239b00cfae7f170e35733002746aa6a8ba5
Rafaela Queiroz <rqueiroz2108@gmail.com>
2025-08-28 08:05:37 -0300

    feat: :sparkles: adiciona função removeUndefined para filtrar propriedades indefinidas de objetos

---
+++ src/utils/object.util.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export function removeUndefined<T extends object>(obj: T): T {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, v]) => v !== undefined),
+  ) as T;
+}
