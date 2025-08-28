cec919a0038cd67a260a9331b713920e05b21f91
Rafaela Queiroz <rqueiroz2108@gmail.com>
2025-08-28 07:48:54 -0300

    feat: :sparkles: adiciona guards e interceptors para controle de acesso e logging no UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,31 +8,44 @@ import {
   Patch,
   Post,
   Put,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UpdatePartiallyUserDto } from './dto/update-partially-user.dto';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
+import { Role } from 'src/enums/role.enum';
+import { Roles } from 'src/decorators/role.decorator';
 
+@UseGuards(AuthGuard, RoleGuard)
+@UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() body: CreateUserDto) {
     return await this.userService.create(body);
   }
 
+  @Roles(Role.Admin, Role.User)
   @Get()
   async read() {
     return await this.userService.read();
   }
 
+  @Roles(Role.Admin, Role.User)
   @Get(':id')
   async readOne(@Param('id', ParseIntPipe) id: number) {
     return await this.userService.readOne(id);
   }
 
+  @Roles(Role.Admin)
   @Put(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -41,6 +54,7 @@ export class UserController {
     return await this.userService.update(id, body);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   async updatePartial(
     @Param('id', ParseIntPipe) id: number,
@@ -49,6 +63,7 @@ export class UserController {
     return await this.userService.updatePartial(id, body);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     return await this.userService.delete(id);
